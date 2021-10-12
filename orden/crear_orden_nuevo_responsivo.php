633b7d0b5b2dfaed24cffb38f674cdb3124a93a9
Alex Rubiano <alexrubianoromero@gmail.com>
2021-10-12 17:41:51 -0500

    mejoras de crear orden respponsivo

---
+++ orden/crear_orden_nuevo_responsivo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">  
+    <title>Document</title>
+    <style>
+     @media (max-width: 600px) {
+        .ingresoInformacion {
+            font-size: 30px;
+            width: 35%;
+            background-color: transparent;
+            color:black;
+            border-color: white; 
+            margin:10px;
+        }
+        .botonResponsivo{
+            font-size: 30px;
+            margin: 10px;
+        }
+    }
+    </style>
+</head>
+<body>
+    <div id= "principal_responsivo" class="container" align = "center">
+        <h1>CREAR ORDEN</h1>
+        <h2>DIGITE LA PLACA </h2>
+        <input type="text" class = "ingresoInformacion"> 
+        <br><BR>
+        <button class="btn btn-primary botonResponsivo">CONSULTAR</button>
+
+    </div>
+</body>
+</html>
