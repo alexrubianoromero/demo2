b470c8ebcc6af8432ee29b4ca4942eeeb3c6e7d3
Alex Rubiano <alexerr@algartech.com>
2023-03-09 13:34:59 -0500

    se bajaron todos los cambios del hosting y se subieron al repositorio2

--- valotablapc.php
+++ valotablapc.php
@@ -42,10 +42,7 @@ $nombrebase = "base_demo";
 
 
 
-$servidor = "localhost";
-$usuario = "ctwtvsxj_admin";
-$clave  = "ElMejorProgramador***";
-$nombrebase = "ctwtvsxj_base_demo";
+
 
 
 $conexion =mysql_connect($servidor,$usuario,$clave);
